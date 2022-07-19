dbe3a234bcc3d294965bc3dd5e3f45c3137f9950
jsduniya <jsduniya747@gmail.com>
2022-07-19 07:11:42 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline, Grid } from '@material-ui/core'
 
-function App() {
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <CssBaseline />
+    <Header />
+    <Grid container spaceing={3} style={{width:'100%'}}>
+        <Grid item xs={12} md={4}>
+            <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+            <Map />
+        </Grid>
+    </Grid>
+    </>
+  )
+};
 
 export default App;
